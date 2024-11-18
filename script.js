5b6fdeed0c350bbf62830c3341620ee8efef28af
Haseeb Malik <haseeb4444059@gmail.com>
2024-11-18 21:45:19 +0500

    Initial commit: Add project files

---
+++ script.js
@@ -0,0 +1,27 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
+const gotoTop = document.querySelector('.go-to-top');
+const mainContent = document.querySelector('.main-content');
+
+hamburgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation()
+    headerContent.classList.add('menu-open')
+})
+
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+window.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+gotoTop.addEventListener('click', () => {
+  mainContent.scrollTo(0,0);
+});
